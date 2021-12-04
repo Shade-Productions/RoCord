9ed4edc8515a78820730ab3ac64cc7eafa156930
Shade <73481203+Shade0122@users.noreply.github.com>
2021-12-04 20:03:05 +0000

    Update and rename RevizAdmin.lua to Coco Hub.lua

---
+++ Scripts/Coco Hub.lua
@@ -0,0 +1,1 @@
+https://raw.githubusercontent.com/Utility-Productions/Utility/main/Scripts/Coco%20Hub.lua
--- Scripts/RevizAdmin.lua
+++
@@ -1,1 +0,0 @@
-https://raw.githubusercontent.com/Utility-Productions/Utility/main/Scripts/Reviz%20Admin.lua
